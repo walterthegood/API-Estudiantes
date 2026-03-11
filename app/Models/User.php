649aa21405bbcd0a1b7c832854aa091054843f4b
walterthegood <waltersalmon2000@gmail.com>
2026-03-11 12:34:23 +0000

    Importo HasApiTokens y modifico el modelo

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
+    use HasApiTokens;
     /**
      * The attributes that are mass assignable.
      *
@@ -22,6 +23,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'age',
+        'grade',
     ];
 
     /**
