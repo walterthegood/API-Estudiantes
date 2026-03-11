873d9e4e03b8317f7cc76e7bfc16e4f2109df578
walterthegood <waltersalmon2000@gmail.com>
2026-03-11 15:27:51 +0000

    Middleware que verifica tu rol al listar /alumnos

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 
 // Esta ruta es pública: http://walter-laravel-11.test/api/login
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    // Ruta protegida por Sanctum Y por nuestro Middleware de Roles
+    Route::get('/alumnos', [StudentController::class, 'index'])
+        ->middleware(CheckRole::class.':ADMIN,PROFESOR'); 
+        
+});
