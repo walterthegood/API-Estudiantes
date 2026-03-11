66637b0fae80b6fe372af7d134107930972d51c2
walterthegood <waltersalmon200@gmail.com>
2026-03-11 22:28:01 +0000

    Middelware del Crud hecho

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,21 @@ use Illuminate\Support\Facades\Route;
 // Esta ruta es pública: http://walter-laravel-11.test/api/login
 Route::post('/login', [AuthController::class, 'login']);
 
+// TODAS las rutas de aquí dentro requieren 
+// que estés logueado (Token Bearer válido)
 Route::middleware(['auth:sanctum'])->group(function () {
     
-    // Ruta protegida por Sanctum Y por nuestro Middleware de Roles
-    Route::get('/alumnos', [StudentController::class, 'index'])
-        ->middleware(CheckRole::class.':ADMIN,PROFESOR'); 
-        
+    // GRUPO 1: ADMIN y PROFESOR (Pueden Ver)
+    Route::middleware(CheckRole::class.':ADMIN,PROFESOR')->group(function () {
+        Route::get('/alumnos', [StudentController::class, 'index']);
+        Route::get('/alumnos/{id}', [StudentController::class, 'show']);
+    });
+
+    // GRUPO 2: SOLO ADMIN (Pueden Modificar)
+    Route::middleware(CheckRole::class.':ADMIN')->group(function () {
+        Route::post('/alumnos', [StudentController::class, 'store']);
+        Route::put('/alumnos/{id}', [StudentController::class, 'update']);
+        Route::delete('/alumnos/{id}', [StudentController::class, 'destroy']);
+    });
+
 });
